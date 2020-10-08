3a0ad9bcc4045b0d16f5f6fec8558952b91d3a8a
sunniraleigh <sraleigh@smith.edu>
2020-10-08 16:58:10 +0000

    modify version2

--- version2/raffle.py
+++ version2/raffle.py
@@ -1,6 +1,11 @@
 """Randomly pick customer and print customer info"""
 
-# Add code starting here
+from random import choice
+import customers
 
-# Hint: remember to import any functions you need from
-# other files or libraries
+def pick_winner(customer_file_path):
+    customers_list = customers.get_customer_from_file(customer_file_path)
+
+    chosen_customer = choice(customers_list)
+
+    print(f"Tell {chosen_customer.name} at {chosen_customer.email} that they've won")
